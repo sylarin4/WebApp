4851c0af2a63d007f72bafce4b11c18e4eb52540
abiesalba <abies_alba@inf.elte.hu>
2020-01-30 18:30:46 +0100

    Init

---
+++ AdventureGameEditor/AdventureGameEditor/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureGameEditor.Models
+{
+    public class User
+    {
+        public String UserName { get; set; }
+        public String NickName { get; set; }
+    }
+}
