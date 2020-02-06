acbafcd99b76c867f3bfa79744775f05425d59bb
abiesalba <abies_alba@inf.elte.hu>
2020-02-06 21:12:38 +0100

    Identity package is added
    TODO: registration fails somehow. (It's acts like invalid parameters added.)

--- AdventureGameEditor/AdventureGameEditor/Data/AdventureGameEditorContext.cs
+++ AdventureGameEditor/AdventureGameEditor/Data/AdventureGameEditorContext.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using AdventureGameEditor.Models;
 
 namespace AdventureGameEditor.Data
 {
-    public class AdventureGameEditorContext : DbContext
+    public class AdventureGameEditorContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
         public AdventureGameEditorContext (DbContextOptions<AdventureGameEditorContext> options)
             : base(options)
